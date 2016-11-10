e315947754ff10e5a4420b64c6ca37327f072991
TitenkovEA <e.a.titenkov@gmail.com>
2016-11-10 04:35:46 +0300

    DDT for task5, with XML data file.

--- UT3/src/test/java/triangle/TriangleDataProviders.java
+++ UT3/src/test/java/triangle/TriangleDataProviders.java
@@ -1,97 +1,91 @@
 package triangle;
 
 import org.testng.annotations.DataProvider;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
 
 public class TriangleDataProviders {
+    private static final String XML_DATA_FILE_PATH = "src" + File.separator +
+            "data" + File.separator + "trianglesData.xml";
+    private static final String NEGATIVE_TRIANGLE = "negative";
+    private static final String SCALENE_TRIANGLE = "scalene";
+    private static final String ISOSCELES_TRIANGLE = "isosceles";
+    private static final String EQUILATERAL_TRIANGLE = "equilateral";
+    private static final String SIDE_A = "side_a";
+    private static final String SIDE_B = "side_b";
+    private static final String SIDE_C = "side_c";
+
     @DataProvider(name = "notValidTriangleSides")
-    public static Object[][] createNotValidTrianglesSides() {
-        return new Object[][]{
-                {Double.NaN, 2, 3},
-                {2, Double.NaN, 3},
-                {2, 3, Double.NaN},
-                {Double.NaN, Double.NaN, Double.NaN},
-                {Double.NaN, 2, Double.NaN},
-                {Double.NaN, Double.NaN, 2},
-                {2, Double.NaN, Double.NaN},
-                {Double.POSITIVE_INFINITY, 2, 3},
-                {2, Double.POSITIVE_INFINITY, 3},
-                {2, 3, Double.POSITIVE_INFINITY},
-                {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-                {2, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
-                {Double.POSITIVE_INFINITY, 2, Double.POSITIVE_INFINITY},
-                {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, 2},
-                {Double.NEGATIVE_INFINITY, 2, 3},
-                {2, Double.NEGATIVE_INFINITY, 3},
-                {2, 3, Double.NEGATIVE_INFINITY},
-                {Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY},
-                {2, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY},
-                {Double.NEGATIVE_INFINITY, 2, Double.NEGATIVE_INFINITY},
-                {Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, 2},
-                {0, 0, 0},
-                {0, 0, 3},
-                {0, 3, 0},
-                {3, 0, 0},
-                {0, 2, 3},
-                {2, 0, 3},
-                {3, 2, 0},
-                {1, 1, Double.MAX_VALUE},
-                {1, Double.MAX_VALUE, 1},
-                {Double.MAX_VALUE, 1, 1},
-                {Double.MIN_VALUE, Double.MIN_VALUE, 1},
-                {Double.MIN_VALUE, 1, Double.MIN_VALUE},
-                {1, Double.MIN_VALUE, Double.MIN_VALUE},
-                {1, 1, -1},
-                {1, -1, 1},
-                {-1, 1, 1},
-                {-1, -1, 1},
-                {1, -1, -1},
-                {-1, 1, -1},
-                {-1, -1, -1},
-                {20, 2, 4},
-                {2, 20, 4},
-                {2, 2, 40},
-                {1, 2, 3},
-                {3, 1, 2},
-                {2, 3, 1}
-        };
+    public static Object[][] createNotValidTrianglesSides()
+            throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = builderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(new File(XML_DATA_FILE_PATH));
+
+        NodeList negativeTriangles = document.getElementsByTagName(NEGATIVE_TRIANGLE);
+        Object[][] result = new Double[negativeTriangles.getLength()][];
+        for (int i = 0; i < negativeTriangles.getLength(); i++) {
+            NamedNodeMap namedNodeMap = negativeTriangles.item(i).getAttributes();
+            result[i] = new Double[]{
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_A).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_B).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_C).getNodeValue()),
+            };
+        }
+
+        return result;
     }
 
-    @DataProvider(name = "scaleneTriangleSides")
-    public static Object[][] createScaleneTriangleSides() {
-        return new Object[][]{
-                {3, 4, 6},
-                {6, 3, 4},
-                {4, 6, 3},
-                {Double.MAX_VALUE, 1.7976931348623155E+308, 1.7976931348623153E+308},
-                {1.7976931348623153E+308, Double.MAX_VALUE, 1.7976931348623155E+308},
-                {1.7976931348623155E+308, 1.7976931348623153E+308, Double.MAX_VALUE},
-                {Double.MIN_VALUE, 2.2250738585072011E-308, 2.2250738585072014E-308},
-                {2.2250738585072014E-308, Double.MIN_VALUE, 2.2250738585072011E-308},
-                {2.2250738585072011E-308, 2.2250738585072014E-308, Double.MIN_VALUE}
-        };
-    }
+    @DataProvider(name = "validTriangleSides")
+    public static Object[][] createValidTrianglesSides()
+            throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = builderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(new File(XML_DATA_FILE_PATH));
 
-    @DataProvider(name = "isoscelesTriangleSides")
-    public static Object[][] createIsoscelesTriangleSides() {
-        return new Object[][]{
-                {2, 3, 3},
-                {3, 3, 2},
-                {3, 2, 3},
-                {1, Double.MAX_VALUE, Double.MAX_VALUE},
-                {Double.MAX_VALUE, 1, Double.MAX_VALUE},
-                {Double.MAX_VALUE, Double.MAX_VALUE, 1},
-                {Double.MIN_VALUE, 1, 1},
-                {1, Double.MIN_VALUE, 1},
-                {1, 1, Double.MIN_VALUE}
-        };
-    }
+        NodeList scaleneTriangles = document.getElementsByTagName(SCALENE_TRIANGLE);
+        NodeList isoscelesTriangles = document.getElementsByTagName(ISOSCELES_TRIANGLE);
+        NodeList equilateralTriangles = document.getElementsByTagName(EQUILATERAL_TRIANGLE);
+        int fullLength = scaleneTriangles.getLength() + isoscelesTriangles.getLength() +
+                equilateralTriangles.getLength();
+        Object[][] result = new Object[fullLength][];
+        for (int i = 0; i < scaleneTriangles.getLength(); i++) {
+            NamedNodeMap namedNodeMap = scaleneTriangles.item(i).getAttributes();
+            result[i] = new Object[]{
+                    SCALENE_TRIANGLE,
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_A).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_B).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_C).getNodeValue()),
+            };
+        }
+        for (int i = 0; i < isoscelesTriangles.getLength(); i++) {
+            NamedNodeMap namedNodeMap = isoscelesTriangles.item(i).getAttributes();
+            result[i + scaleneTriangles.getLength()] = new Object[]{
+                    ISOSCELES_TRIANGLE,
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_A).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_B).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_C).getNodeValue()),
+            };
+        }
+        for (int i = 0; i < equilateralTriangles.getLength(); i++) {
+            NamedNodeMap namedNodeMap = equilateralTriangles.item(i).getAttributes();
+            result[i + scaleneTriangles.getLength() +
+                    isoscelesTriangles.getLength()] = new Object[]{
+                    EQUILATERAL_TRIANGLE,
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_A).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_B).getNodeValue()),
+                    Double.parseDouble(namedNodeMap.getNamedItem(SIDE_C).getNodeValue()),
+            };
+        }
 
-    @DataProvider(name = "equilateralTriangleSides")
-    public static Object[][] createEquilateralTriangleSides() {
-        return new Object[][]{
-                {3, 3, 3},
-                {Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE},
-                {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE}
-        };
+        return result;
     }
 }
--- UT3/src/test/java/triangle/TriangleTest.java
+++ UT3/src/test/java/triangle/TriangleTest.java
@@ -7,25 +7,16 @@ import static org.testng.Assert.*;
 public class TriangleTest {
     @Test(dataProvider = "notValidTriangleSides", dataProviderClass = TriangleDataProviders.class,
             expectedExceptions = Exception.class)
-    public void negativeTriangleConstructor(double sideA, double sideB, double sideC) throws Exception {
+    public void negativeTriangleConstructor(double sideA, double sideB, double sideC)
+            throws Exception {
         new Triangle(sideA, sideB, sideC);
     }
 
-    @Test(dataProvider = "scaleneTriangleSides", dataProviderClass = TriangleDataProviders.class)
-    public void positiveGetTriangleTypeOfScalene(double sideA, double sideB, double sideC) throws Exception {
+    @Test(dataProvider = "validTriangleSides", dataProviderClass = TriangleDataProviders.class)
+    public void positiveGetTriangleTypeOfScalene(String triangleType,
+                                                 double sideA, double sideB, double sideC)
+            throws Exception {
         Triangle triangle = new Triangle(sideA, sideB, sideC);
-        assertEquals(triangle.getTriangleType(), Triangle.SCALENE_TYPE);
-    }
-
-    @Test(dataProvider = "isoscelesTriangleSides", dataProviderClass = TriangleDataProviders.class)
-    public void positiveGetTriangleTypeOfIsosceles(double sideA, double sideB, double sideC) throws Exception {
-        Triangle triangle = new Triangle(sideA, sideB, sideC);
-        assertEquals(triangle.getTriangleType(), Triangle.ISOSCELES_TYPE);
-    }
-
-    @Test(dataProvider = "equilateralTriangleSides", dataProviderClass = TriangleDataProviders.class)
-    public void positiveGetTriangleTypeOfEquilatera(double sideA, double sideB, double sideC) throws Exception {
-        Triangle triangle = new Triangle(sideA, sideB, sideC);
-        assertEquals(triangle.getTriangleType(), Triangle.EQUILATERAL_TYPE);
+        assertEquals(triangle.getTriangleType(), triangleType);
     }
 }
